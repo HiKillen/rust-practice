127e1c488691a4463fcba0335169629323d7054f
HiKillen <133752512+HiKillen@users.noreply.github.com>
2025-03-20 13:45:59 +0200

    Update hw03.rs

---
+++ src/homework/hw03.rs
@@ -0,0 +1,24 @@
+const WIDTH: usize = 30;
+const HEIGHT: usize = 15;
+
+fn main() {
+    let mut output = String::new();
+
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            // Малюємо рамку
+            if y == 0 || y == HEIGHT - 1 || x == 0 || x == WIDTH - 1 {
+                output.push('*');
+            }
+            // Малюємо діагоналі (лінії конверта)
+            else if x == y * (WIDTH - 1) / (HEIGHT - 1) || x == (WIDTH - 1) - y * (WIDTH - 1) / (HEIGHT - 1) {
+                output.push('*');
+            } else {
+                output.push(' ');
+            }
+        }
+        output.push('\n');
+    }
+
+    print!("{}", output);
+}
